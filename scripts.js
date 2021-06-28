2870ada6b20219e44b72af83b5cb88a4aa40d8ce
razieh boroon <razieh.boroon@gmail.com>
2021-06-28 12:28:06 +0430

    first edition - mess

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const navBtn = document.querySelector(".btn");
+const menuSlide = document.querySelector("#menu");
+
+
+navBtn.addEventListener("click", () => {
+  menuSlide.classList.toggle("navToggle");
+
+});
+
+
