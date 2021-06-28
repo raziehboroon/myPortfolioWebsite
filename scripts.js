d84f57b7305de2355c9182e0e6781162459d524f
razieh boroon <razieh.boroon@gmail.com>
2021-06-28 20:16:15 +0430

    before navbar changes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
 const navBtn = document.querySelector(".btn");
 const menuSlide = document.querySelector("#menu");
-
+const navLinks = document.querySelectorAll("nav ul a");
 
 navBtn.addEventListener("click", () => {
   menuSlide.classList.toggle("navToggle");
-
 });
 
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    menuSlide.classList.toggle("navToggle");
+  });
+});
 
+$("nav a").on("click", function (e) {
+  if (this.hash != "") {
+    e.preventDefault();
+    const hash = this.hash;
+    $("html, body").animate({ scrollTop: $(hash).offset().top }, 800);
+  }
+});
