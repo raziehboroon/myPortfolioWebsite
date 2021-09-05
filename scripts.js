6eefe9cc5ebab96f5626d3be6cce1a0ca9855ed3
razieh boroon <razieh.boroon@gmail.com>
2021-09-05 15:56:04 +0430

    some edit

--- scripts.js
+++ scripts.js
@@ -1,22 +1,52 @@
+const nav = document.querySelector("nav");
 const navBtn = document.querySelector(".navBtn");
 const menuSlide = document.querySelector("#menu");
-const navLinks = document.querySelectorAll("nav ul a");
+// const navLinks = document.querySelectorAll("nav ul a");
+const navLogo = document.querySelector(".nav-logo");
+const copyrightDate = document.querySelector("#copyright-date");
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    nav.classList.add("nav-fixed");
+  } else {
+    nav.classList.remove("nav-fixed");
+  }
+});
 
 navBtn.addEventListener("click", () => {
   menuSlide.classList.toggle("navToggle");
   navBtn.classList.toggle("navToggle");
 });
 
-navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", () => {
-    menuSlide.classList.toggle("navToggle");
-  });
+navLogo.addEventListener("click", () => {
+  menuSlide.classList.remove("navToggle");
+  navBtn.classList.remove("navToggle");
 });
 
+menuSlide.addEventListener("click", () => {
+  menuSlide.classList.remove("navToggle");
+  navBtn.classList.remove("navToggle");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 776) {
+    menuSlide.classList.remove("navToggle");
+  }
+});
+const getData = () => {
+  const date = new Date();
+  const fullYear = date.getFullYear();
+  return fullYear;
+};
+copyrightDate.textContent = getData();
+
+// when click on nav item, scroll to top of that section
 $("nav a").on("click", function (e) {
   if (this.hash != "") {
     e.preventDefault();
     const hash = this.hash;
-    $("html, body").animate({ scrollTop: $(hash).offset().top }, 800);
+    const navHeight = nav.offsetHeight;
+    var scrollToPosition = $(hash).offset().top - navHeight;
+    $("html, body").animate({ scrollTop: scrollToPosition }, 800);
   }
 });
